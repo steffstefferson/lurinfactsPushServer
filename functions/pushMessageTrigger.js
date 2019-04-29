013aeeaf9b96e9170642520e52921b4a0f7c27d1
Stef Käser <info.stef.kaeser@gmail.com>
2019-04-29 22:06:04 +0200

    fixing unit tests

--- functions/pushMessageTrigger.js
+++ functions/pushMessageTrigger.js
@@ -1,160 +1,235 @@
+module.exports = function(refFirebase, webpushHelper, utility) {
+  var firebase = refFirebase;
+  // if no utility is passed we use this default utility
+  var defaultUtils = {
+    isLurinday: function() {
+      return new Date().getMonth() == 9 && new Date().getDate() == 24;
+    },
+    randomBool: function() {
+      return +new Date() % 2;
+    }
+  };
+  var utils = utility || defaultUtils;
 
-module.exports = function (refFirebase, webpushHelper, utility) {
-    var firebase = refFirebase;
-    // if no utility is passed we use this default utility
-    var defaultUtils = {
-        isLurinday: function () { return new Date().getMonth() == 9 && new Date().getDate() == 24 },
-        randomBool: function () { return +new Date() % 2; }
-    };
-    var utils = utility || defaultUtils;
-
-    const isTimeToRun = function (pushState, checkIsTimeToRun) {
-        if (!pushState || !pushState.lastPushMessage) {
-            return { isTimeToRun: false };
-        }
-
-        //define min time diffrence between push message.
-        const minTimeDiffSecondsNewContent = pushState && pushState.pushNewContentEveryXHours * 3600 || 3600 * 24;
-        const minTimeDiffSecondsRandomContent = pushState && pushState.pushRandomContentEveryXDays * 24 * 3600 || 3600 * 24;
-        const minTimeToLurinDay = 3600 * 24;
-
-        var isTime = function (timeDiff) {
-            return (new Date().getTime() - pushState.lastPushMessage) / 1000 > timeDiff;
-        };
-
-        const isTimeToPushNew = !checkIsTimeToRun || isTime(minTimeDiffSecondsNewContent);
-        const isTimeToPushRandom = !checkIsTimeToRun || isTime(minTimeDiffSecondsRandomContent);
-        const isTimeToLurinDay = isTime(minTimeToLurinDay);
-
-        var isTimeToRun = isTimeToPushNew || isTimeToPushRandom || isTimeToLurinDay;
-        return { isTimeToRun, isTimeToPushRandom, isTimeToPushNew, isTimeToLurinDay };
-    };
-
-    const getNewContentToPush = function (pushState) {
-        return firebase.getNewestFact(pushState.lastFactDate).then(function (fact) {
-            if (fact) {
-                return {
-                    pushMessage: createPushMsg('newfact', 'New wisdom about lurin', fact.fact + ' | by ' + fact.contributor, null, fact.itemKey),
-                    updateFn: function (state) {
-                        state.lastFactDate = fact.insertTime;
-                        return state;
-                    }
-                };
-            }
-            return firebase.getNewestImage(pushState.lastImageDate).then(function (image) {
-                if (image) {
-                    return {
-                        pushMessage: createPushMsg('newimage', 'Lurin is on another trip!', image.imageTitle + '\n ' + image.funFact, null, image.imageKey),
-                        updateFn: function (state) {
-                            state.lastImageDate = image.insertTime;
-                            return state;
-                        }
-                    };
-                }
-                return null;
-            });
-        });
-    };
-
-    const getRandomContentToPush = function (content) {
-        if (utils.randomBool()) {
-            return firebase.getRandomImage().then(function (image) {
-                if (image) {
-                    return {
-                        pushMessage: createPushMsg('randomimage', 'Have you already been in?', image.imageTitle + '\n ' + image.funFact, null, image.imageKey),
-                        updateFn: function (state) {
-                            state.randomImage = state.randomImage || [];
-                            state.randomImage.push(image.imageKey);
-                            return state;
-                        }
-                    };
-                }
-                return null;
-            });
-        }
-        return firebase.getRandomFact().then(function (fact) {
-            if (fact) {
-                return {
-                    pushMessage: createPushMsg('randomfact', 'Did you know?', fact.fact + ' | by ' + fact.contributor, null, fact.itemKey),
-                    updateFn: function (state) {
-                        state.randomFact = state.randomFact || [];
-                        state.randomFact.push(fact.itemKey);
-                        return state;
-                    }
-                };
-            }
-            return null;
-        })
-
-    };
-
-    const createPushMsg = function (type, title, message, imageLink, itemKey) {
-        return { title, message, type, imageLink, itemKey };
+  const isTimeToRun = function(pushState, checkIsTimeToRun) {
+    if (!pushState || !pushState.lastPushMessage) {
+      return { isTimeToRun: false };
     }
 
-    const pushSomeContentInternal = function (pushState, timeToRunResult) {
-        if (timeToRunResult.isTimeToLurinDay && utils.isLurinday()) {
-            var msg = { pushMessage: createPushMsg('lurinday', 'Happy lurinday everyone', 'have a nice day!') };
-            return webpushHelper.pushContent(msg);
-        }
+    //define min time diffrence between push message.
+    const minTimeDiffSecondsNewContent =
+      (pushState && pushState.pushNewContentEveryXHours * 3600) || 3600 * 24;
+    const minTimeDiffSecondsRandomContent =
+      (pushState && pushState.pushRandomContentEveryXDays * 24 * 3600) ||
+      3600 * 24;
+    const minTimeToLurinDay = 3600 * 24;
 
-        return getNewContentToPush(pushState).then(function (content) {
-            if (!timeToRunResult.isTimeToPushNew || !content) {
-                return timeToRunResult.isTimeToPushRandom && getRandomContentToPush(pushState);
-            }
-            return content;
-        }).then(function (content) {
-            if (content) {
-                console.log('gotAnyContent to push: ' + content.pushMessage.type + ' title : ' + content.pushMessage.title);
-                return webpushHelper.pushContent(content.pushMessage).then(function (subscriptions) {
-                    return { subscriptions, content };
-                });
-            } else {
-                return Promise.resolve({ result: 'nocontenttopush' });
-            }
-        });
+    var isTime = function(timeDiff) {
+      return (
+        (new Date().getTime() - pushState.lastPushMessage) / 1000 > timeDiff
+      );
     };
 
-    const pushSomeContent = function (skipTimeCheckKey) {
+    const isTimeToPushNew =
+      !checkIsTimeToRun || isTime(minTimeDiffSecondsNewContent);
+    const isTimeToPushRandom =
+      !checkIsTimeToRun || isTime(minTimeDiffSecondsRandomContent);
+    const isTimeToLurinDay = isTime(minTimeToLurinDay);
 
-        return firebase.getPushState().then(function (pushState) {
+    var isTimeToRun = isTimeToPushNew || isTimeToPushRandom || isTimeToLurinDay;
+    return {
+      isTimeToRun,
+      isTimeToPushRandom,
+      isTimeToPushNew,
+      isTimeToLurinDay
+    };
+  };
 
-            if (!pushState || !pushState.lastPushMessage) {
-                return Promise.resolve({ result: 'nopushstate' });
-            }
-            skipTimeCheckKey && console.log('skipTimeCheckKey: ' + skipTimeCheckKey);
-            var checkTimeToRun = !(pushState.skipTimeCheckKey && skipTimeCheckKey == pushState.skipTimeCheckKey);
-            var timeToRunResult = isTimeToRun(pushState, checkTimeToRun);
-            if (!timeToRunResult.isTimeToRun) {
-                return Promise.resolve({ result: 'nottimetorun' });
+  const getNewContentToPush = function(pushState) {
+    return firebase.getNewestFact(pushState.lastFactDate).then(function(fact) {
+      if (fact) {
+        return {
+          pushMessage: createPushMsg(
+            "newfact",
+            "New wisdom about lurin",
+            fact.fact + " | by " + fact.contributor,
+            null,
+            fact.itemKey
+          ),
+          updateFn: function(state) {
+            state.lastFactDate = fact.insertTime;
+            return state;
+          }
+        };
+      }
+      return firebase
+        .getNewestImage(pushState.lastImageDate)
+        .then(function(image) {
+          if (image) {
+            return {
+              pushMessage: createPushMsg(
+                "newimage",
+                "Lurin is on another trip!",
+                image.imageTitle + "\n " + image.funFact,
+                null,
+                image.imageKey
+              ),
+              updateFn: function(state) {
+                state.lastImageDate = image.insertTime;
+                return state;
+              }
             };
-            console.log('its time to push something');
-
-            pushState.lastPushMessage = +new Date();
-            return firebase.updatePushState(pushState).then(function () {
-                return pushSomeContentInternal(pushState, timeToRunResult).then(function (result) {
-                    const content = result.content;
-                    if (content) {
-                        var newPushState = content.updateFn ? content.updateFn(pushState) : pushState;
-                        return firebase.updatePushState(newPushState).then(function (updateResult) {
-                            return Promise.resolve({ result: 'contentpushed', content: content });
-                        });
-                    }
-                    return Promise.resolve({ result: 'nocontent' });
-                });
-            });
+          }
+          return null;
         });
-    };
+    });
+  };
 
-    const pushMessage = function (title, message, receiver) {
-        var msg = createPushMsg('custom', title, message);
-        return webpushHelper.pushContent(msg);
-    };
+  const getRandomContentToPush = function(content) {
+    if (utils.randomBool()) {
+      return firebase.getRandomImage().then(function(image) {
+        if (image) {
+          return {
+            pushMessage: createPushMsg(
+              "randomimage",
+              "Have you already been in?",
+              image.imageTitle + "\n " + image.funFact,
+              null,
+              image.imageKey
+            ),
+            updateFn: function(state) {
+              state.randomImage = state.randomImage || [];
+              state.randomImage.push(image.imageKey);
+              return state;
+            }
+          };
+        }
+        return null;
+      });
+    }
+    return firebase.getRandomFact().then(function(fact) {
+      if (fact) {
+        return {
+          pushMessage: createPushMsg(
+            "randomfact",
+            "Did you know?",
+            fact.fact + " | by " + fact.contributor,
+            null,
+            fact.itemKey
+          ),
+          updateFn: function(state) {
+            state.randomFact = state.randomFact || [];
+            state.randomFact.push(fact.itemKey);
+            return state;
+          }
+        };
+      }
+      return null;
+    });
+  };
 
-    const pushMessageTo = function (title, message, receiver) {
-        var msg = createPushMsg('custom', title, message);
-        return webpushHelper.pushContentTo(msg, receiver);
-    };
+  const createPushMsg = function(type, title, message, imageLink, itemKey) {
+    return { title, message, type, imageLink, itemKey };
+  };
 
-    return { isTimeToRun, getNewContentToPush, pushSomeContent, pushMessage, pushMessageTo };
+  const pushSomeContentInternal = function(pushState, timeToRunResult) {
+    return getContentInternal(pushState, timeToRunResult).then(content => {
+      if (content && content.pushMessage) {
+        console.log(
+          "gotAnyContent to push: " +
+            content.pushMessage.type +
+            " title : " +
+            content.pushMessage.title
+        );
+        return webpushHelper
+          .pushContent(content.pushMessage)
+          .then(function(subscriptions) {
+            return { subscriptions, content };
+          });
+      } else {
+        return Promise.resolve({ result: "nocontenttopush" });
+      }
+    });
+  };
+
+  const getContentInternal = function(pushState, timeToRunResult) {
+    if (timeToRunResult.isTimeToLurinDay && utils.isLurinday()) {
+      var msg = {
+        pushMessage: createPushMsg(
+          "lurinday",
+          "Happy lurinday everyone",
+          "have a nice day!"
+        )
+      };
+      return Promise.resolve(msg);
+    }
+    if (timeToRunResult.isTimeToPushNew) {
+      return getNewContentToPush(pushState).then(content => {
+        if (!content && timeToRunResult.isTimeToPushRandom) {
+          return getRandomContentToPush(pushState);
+        }
+        return content;
+      });
+    }
+    return getRandomContentToPush(pushState);
+  };
+
+  const pushSomeContent = function(skipTimeCheckKey) {
+    return firebase.getPushState().then(function(pushState) {
+      if (!pushState || !pushState.lastPushMessage) {
+        return Promise.resolve({ result: "nopushstate" });
+      }
+      skipTimeCheckKey && console.log("skipTimeCheckKey: " + skipTimeCheckKey);
+      var checkTimeToRun = !(
+        pushState.skipTimeCheckKey &&
+        skipTimeCheckKey == pushState.skipTimeCheckKey
+      );
+      var timeToRunResult = isTimeToRun(pushState, checkTimeToRun);
+      if (!timeToRunResult.isTimeToRun) {
+        return Promise.resolve({ result: "nottimetorun" });
+      }
+      console.log("its time to push something");
+
+      pushState.lastPushMessage = +new Date();
+      return firebase.updatePushState(pushState).then(function() {
+        return pushSomeContentInternal(pushState, timeToRunResult).then(
+          function(result) {
+            const pushMessage = result.content && result.content.pushMessage;
+            if (pushMessage) {
+              var newPushState = pushMessage.updateFn
+                ? pushMessage.updateFn(pushState)
+                : pushState;
+              return firebase
+                .updatePushState(newPushState)
+                .then(function(updateResult) {
+                  return Promise.resolve({
+                    result: "contentpushed",
+                    pushMessage: pushMessage
+                  });
+                });
+            }
+            return Promise.resolve({ result: "nocontent" });
+          }
+        );
+      });
+    });
+  };
+
+  const pushMessage = function(title, message, receiver) {
+    var msg = createPushMsg("custom", title, message);
+    return webpushHelper.pushContent(msg);
+  };
+
+  const pushMessageTo = function(title, message, receiver) {
+    var msg = createPushMsg("custom", title, message);
+    return webpushHelper.pushContentTo(msg, receiver);
+  };
+
+  return {
+    isTimeToRun,
+    getNewContentToPush,
+    pushSomeContent,
+    pushMessage,
+    pushMessageTo
+  };
 };
---
+++ spec/someContentToPush.spec.js
@@ -0,0 +1,144 @@
+const webpush_Mock = {
+  setVapidDetails: function() {},
+  sendNotification: function(receiver, data) {
+    console.log("Send webpush to " + receiver.id, [receiver, data]);
+    return Promise.resolve();
+  }
+};
+
+var msgs = [];
+
+const webpushHelper = {
+  pushContent: function(msg) {
+    msgs.push(msg);
+    return Promise.resolve([{ subscriber: "A" }, { subscriber: "B" }]);
+  }
+};
+
+var pushStates = [];
+
+var beforeYesterday = new Date();
+beforeYesterday.setDate(beforeYesterday.getDate() - 2);
+
+const firebase = {
+  getPushState: function() {
+    return Promise.resolve({ lastPushMessage: beforeYesterday });
+  },
+  updatePushState: function(state) {
+    pushStates.push(state);
+    return Promise.resolve(state);
+  },
+  getRandomFact: function() {
+    return Promise.resolve({ title: "test" });
+  }
+};
+
+var helper = { makeObjectNot: "false" };
+
+var triggerLogic = require("./../functions/pushMessageTrigger.js")(
+  firebase,
+  webpushHelper,
+  helper
+);
+
+var assert = require("assert");
+
+describe("#SomeContentToPush", function() {
+  beforeEach(function() {
+    msgs = [];
+    firebase.getSubscriptionsFromDatabase = function() {
+      return Promise.resolve({});
+    };
+    firebase.getCustomMesage = function() {
+      return Promise.resolve(null);
+    };
+    firebase.getNewestFact = function(d) {
+      return Promise.resolve(null);
+    };
+    firebase.getNewestImage = function(d) {
+      return Promise.resolve(null);
+    };
+    helper.isLurinday = function() {
+      return false;
+    };
+    helper.randomBool = function() {
+      return false;
+    };
+    pushStates = [];
+  });
+
+  it("should push new fact content", async function() {
+    firebase.getNewestFact = function(d) {
+      return Promise.resolve({ insertTime: new Date(), fact: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(msgs.length, 1);
+    assert.equal(msgs[0].type, "newfact");
+  });
+
+  it("should push new image content", async function() {
+    firebase.getNewestImage = function(d) {
+      return Promise.resolve({ insertTime: new Date(), imageTitle: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(msgs.length, 1);
+    assert.equal(msgs[0].type, "newimage");
+  });
+
+  it("should be lurin day", async function() {
+    helper.isLurinday = function() {
+      return true;
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(msgs.length, 1);
+    assert.equal(msgs[0].type, "lurinday");
+  });
+
+  it("should push random fact", async function() {
+    helper.randomBool = function() {
+      return false;
+    };
+    firebase.getRandomFact = function(d) {
+      return Promise.resolve({ insertTime: new Date(), fact: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(msgs.length, 1);
+    assert.equal(msgs[0].type, "randomfact");
+  });
+
+  it("should push random image", async function() {
+    helper.randomBool = function() {
+      return true;
+    };
+    firebase.getRandomImage = function(d) {
+      return Promise.resolve({ insertTime: new Date(), imageTitle: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(msgs.length, 1);
+    assert.equal(msgs[0].type, "randomimage");
+  });
+
+  it("pushstate was updated twice", async function() {
+    helper.randomBool = function() {
+      return true;
+    };
+    firebase.getRandomImage = function(d) {
+      return Promise.resolve({ insertTime: new Date(), imageTitle: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(pushStates.length, 2);
+  });
+
+  it("pushstate has an acutal lastPushDate", async function() {
+    var now = new Date();
+    helper.randomBool = function() {
+      return true;
+    };
+    firebase.getRandomImage = function(d) {
+      return Promise.resolve({ insertTime: new Date(), imageTitle: "blabla" });
+    };
+    var a = await triggerLogic.pushSomeContent(false);
+    assert.equal(pushStates[0].lastPushMessage >= now, true);
+    assert.equal(pushStates[1].lastPushMessage >= now, true);
+  });
+});
